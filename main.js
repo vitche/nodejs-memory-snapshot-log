163f61c93dbd4aba64299831dc572f8f6f495ce7
Vitche Research Team <developer@vitche.com>
2020-03-17 11:08:01 -0400

    1. Formatting improved; 2. Ignored Idea files.

--- main.js
+++ main.js
@@ -4,35 +4,35 @@ let heapdump = require('heapdump');
 let parser = require('heapsnapshot-parser');
 
 module.exports = {
-	snapshot: function(path, callback) {
-		let name = path + '/' + uuid() + '.heapsnapshot';
-		heapdump.writeSnapshot(name, (error) => {
-			callback(error, name);
-		});
-	},
-	log: function(path, level) {
-		let content = fs.readFileSync(path, {
-			encoding: 'utf-8'
-		});
-		let snapshot = parser.parse(content);
-		delete content;
+    snapshot: function (path, callback) {
+        let name = path + '/' + uuid() + '.heapsnapshot';
+        heapdump.writeSnapshot(name, (error) => {
+            callback(error, name);
+        });
+    },
+    log: function (path, level) {
+        let content = fs.readFileSync(path, {
+            encoding: 'utf-8'
+        });
+        let snapshot = parser.parse(content);
+        delete content;
 
-		let distribution = {};
-		for (let i = 0; i < snapshot.nodes.length; i++) {
-			let node = snapshot.nodes[i];
-			if (!distribution[node.name]) {
-				distribution[node.name] = 0;
-			}
-			distribution[node.name] += node.self_size;
-		}
+        let distribution = {};
+        for (let i = 0; i < snapshot.nodes.length; i++) {
+            let node = snapshot.nodes[i];
+            if (!distribution[node.name]) {
+                distribution[node.name] = 0;
+            }
+            distribution[node.name] += node.self_size;
+        }
 
-		let filteredDistribution = {};
-		for (let item in distribution) {
-			if (level < distribution[item]) {
-				filteredDistribution[item] = distribution[item];
-			}
-		}
+        let filteredDistribution = {};
+        for (let item in distribution) {
+            if (level < distribution[item]) {
+                filteredDistribution[item] = distribution[item];
+            }
+        }
 
-		console.log('Memory snapshot', JSON.stringify(filteredDistribution));
-	}
+        console.log('Memory snapshot', JSON.stringify(filteredDistribution));
+    }
 };
